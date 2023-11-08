bcb061b2b090ef03f42ddf597ef0dea14f0ebae7
Sam Erika Clotfelter <g@samclot.com>
2023-11-07 20:04:02 -0500

    start_*.py now source the virtualenv

--- start_celery.py
+++ start_celery.py
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
+source venv/bin/activate
 cd src
 celery -A start.celery beat -s /tmp/co2-celerybeat-schedule
--- start_flask.py
+++ start_flask.py
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
+source venv/bin/activate
 cd src/
 FLASK_APP=start.py flask run --host=0.0.0.0 --debug
